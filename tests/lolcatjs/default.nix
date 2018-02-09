c3f18b8bb470f66c82e52f01cfef177a864638ad
adisbladis <adis@blad.is>
2018-02-09 18:59:18 +0800

    Tests: Add lolcatjs test

---
+++ tests/lolcatjs/default.nix
@@ -0,0 +1,15 @@
+with (import <nixpkgs> {});
+with (import ../../. { inherit pkgs; });
+
+mkPnpmPackage {
+
+  src = fetchFromGitHub {
+    owner = "robertboloc";
+    repo = "lolcatjs";
+    rev = "a0baef18de64de2a794e1726fed89ad6b581aeec";
+    sha256 = "1s922sy2irzjwj8xswanqd6q6dnwaxy252npq4h13yvx7dirgm31";
+  };
+
+  packageJSON = ./package.json;
+  shrinkwrapYML = ./shrinkwrap.yaml;
+}
